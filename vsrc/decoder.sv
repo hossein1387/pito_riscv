814c22a7e5b07fd95828bed906889fbededfb355
hossein1387 <m.h.askari.hemmat@gmail.com>
2019-07-18 01:39:58 -0400

    adding decoder code

--- vsrc/decoder.sv
+++ vsrc/decoder.sv
@@ -1,10 +1,131 @@
 `include "types.svh"
 `include "instr.svh"
+`include "rv_defines.svh"
 
 module decoder (
-    input clk,    // Clock
-    input i_data, // Clock Enable
-    i
+    input                      clk,
+    input                      rst,
+    input                      flush,
+    input                      halt, 
+    input riscv_instr_t        instr, // input instruction
+    input rv_pc_cnt_t          pc,
+
+    output alu_opcode_t        alu_opcode,
+    output rv_register_t       rv_rs1,
+    output rv_register_t       rv_rs2,
+    output rv_register_t       rv_rd,
+
+    output rv_imm_t            rv_imm,
+
+    output rv32_opcode_enum_t  rv_opcode,
+    output logic               instr_trap
+
 );
+
+    rv32_type_enum_t rv_imm_decoded_type;
+    rv_imm_t         rv_imm_decoded;
+
+    always @(posedge clk) begin
+        rv_imm_decoded_type <= RV32_TYPE_UNKNOWN;
+        case (instr[6:0])
+            7'b0110111: begin // LUI
+                rv_imm_decoded_type <= RV32_TYPE_U;
+                rv_rd               <= instr[11:7];
+                rv_opcode           <= RV32_LUI;
+            end
+            7'b0010111: begin // AUIPC
+                rv_imm_decoded_type <= RV32_TYPE_U;
+                rv_rd               <= instr[11:7];
+                rv_opcode           <= RV32_AUIPC;
+            end
+            7'b1101111: begin // JAL
+                rv_imm_decoded_type <= RV32_TYPE_J;
+                rv_rd               <= instr[11:7];
+                rv_opcode           <= RV32_JAL;
+            end
+            7'b1100111: begin // JALR
+                rv_imm_decoded_type <= RV32_TYPE_I;
+                rv_rd               <= instr[11: 7];
+                rv_rs1              <= instr[19:15];
+                rv_opcode           <= RV32_JALR;
+            end
+            7'b1100011: begin // BEQ, BNE, BLT, BGE, BLTU, BGEU
+                rv_imm_decoded_type <= RV32_TYPE_B;
+                rv_rs2              <= instr[24:20];
+                rv_rs1              <= instr[19:15];
+                case (instr[14:12])
+                    3'b000        : rv_opcode <= RV32_BEQ;
+                    3'b001        : rv_opcode <= RV32_BNE;
+                    3'b010, 3'b011: rv_opcode <= RV32_UNKNOWN;
+                    3'b100        : rv_opcode <= RV32_BLT;
+                    3'b101        : rv_opcode <= RV32_BGE;
+                    3'b110        : rv_opcode <= RV32_BLTU;
+                    3'b111        : rv_opcode <= RV32_BGEU;
+                    default       : rv_opcode <= RV32_UNKNOWN;
+                endcase
+            end
+            7'b0000011: begin // LB, LH, LW, LBU, LHU
+                rv_imm_decoded_type <= RV32_TYPE_I;
+                rv_rd               <= instr[11: 7];
+                rv_rs1              <= instr[19:15];
+                case (instr[14:12])
+                    3'b000                 : rv_opcode <= RV32_LB;
+                    3'b001                 : rv_opcode <= RV32_LH;
+                    3'b010                 : rv_opcode <= RV32_LW;
+                    3'b100                 : rv_opcode <= RV32_LBU;
+                    3'b101                 : rv_opcode <= RV32_LHU;
+                    3'b011, 3'b110, 3'b111 : rv_opcode <= RV32_UNKNOWN;
+                    default                : rv_opcode <= RV32_UNKNOWN;
+                endcase
+            end
+            7'b0100011: begin // SB, SH, SW
+                rv_imm_decoded_type <= RV32_TYPE_S;
+                rv_rs2              <= instr[24:20];
+                rv_rs1              <= instr[19:15];
+                case (instr[14:12])
+                    3'b000                 : rv_opcode <= RV32_SB;
+                    3'b001                 : rv_opcode <= RV32_SH;
+                    3'b010                 : rv_opcode <= RV32_SW;
+                    3'b011, 3'b100, 3'b101, 
+                    3'b110, 3'b111         : rv_opcode <= RV32_UNKNOWN;
+                    default                : rv_opcode <= RV32_UNKNOWN;
+                endcase
+            end
+            7'b0010011: begin // ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
+                rv_imm_decoded_type <= RV32_TYPE_I;
+                rv_rs1              <= instr[19:15];
+                case (instr[14:12])
+                    3'b000                 : rv_opcode <= RV32_ADDI;
+                    3'b010                 : rv_opcode <= RV32_SLTI;
+                    3'b010                 : rv_opcode <= RV32_SW;
+                    3'b001, 3'b100, 3'b101, 
+                    3'b110, 3'b111         : rv_opcode <= RV32_UNKNOWN;
+                    default                : rv_opcode <= RV32_UNKNOWN;
+                endcase
+            end
+            7'b0110011: begin // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
+                rv_imm_decoded_type <= RV32_TYPE_R;
+            end
+            7'b0001111: begin // FENCE, FENCEI
+                rv_imm_decoded_type <= RV32_TYPE_I;
+            end
+            7'b1110011: begin // ECALL, EBREAK, CSRRW, CSRRS, CSRRC, CSRRWI, CSRRSI, CSRRCI
+                rv_imm_decoded_type <= RV32_TYPE_I;
+            end
+            default   : begin
+                rv_imm_decoded_type <= RV32_TYPE_UNKNOWN;
+            end /* default */
+        endcase
+    end
+
+
+    imm_gen imm_gen_inst(   .rv_instr     (instr),
+                            .rv_imm_type  (rv_imm_decoded_type),
+                            .rv_imm       (rv_imm_decoded)
+                        );
+
+
+    assign rv_imm     = rv_imm_decoded;
+    assign instr_trap = (rv_imm_decoded_type == RV32_TYPE_UNKNOWN) ? 1'b1 : 1'b0;
 
 endmodule
--- vsrc/instr.svh
+++ vsrc/instr.svh
@@ -1,62 +1,68 @@
+// All riscv32i instructions.
+// Note, not all instructions listed below are implemented by
+// pito riscv. A "*" near the instruction means the instruction
+// is not implemented.
+
+
 // Loads
-`define LB     32'hxxxxxxxxxxxxxxxxx000xxxxx0000011
-`define LH     32'hxxxxxxxxxxxxxxxxx001xxxxx0000011
-`define LW     32'hxxxxxxxxxxxxxxxxx010xxxxx0000011
-`define LBU    32'hxxxxxxxxxxxxxxxxx100xxxxx0000011
-`define LHU    32'hxxxxxxxxxxxxxxxxx101xxxxx0000011
+`define RV32_LB     32'hxxxxxxxxxxxxxxxxx000xxxxx0000011  // *
+`define RV32_LH     32'hxxxxxxxxxxxxxxxxx001xxxxx0000011  // *
+`define RV32_LW     32'hxxxxxxxxxxxxxxxxx010xxxxx0000011  // *
+`define RV32_LBU    32'hxxxxxxxxxxxxxxxxx100xxxxx0000011  // *
+`define RV32_LHU    32'hxxxxxxxxxxxxxxxxx101xxxxx0000011  // *
 // Stores
-`define SB     32'hxxxxxxxxxxxxxxxxx000xxxxx0100011
-`define SH     32'hxxxxxxxxxxxxxxxxx001xxxxx0100011
-`define SW     32'hxxxxxxxxxxxxxxxxx010xxxxx0100011
+`define RV32_SB     32'hxxxxxxxxxxxxxxxxx000xxxxx0100011  // *
+`define RV32_SH     32'hxxxxxxxxxxxxxxxxx001xxxxx0100011  // *
+`define RV32_SW     32'hxxxxxxxxxxxxxxxxx010xxxxx0100011  // *
 // Shifts
-`define SLL    32'h0000000xxxxxxxxxx001xxxxx0110011
-`define SLLI   32'h0000000xxxxxxxxxx001xxxxx0010011
-`define SRL    32'h0000000xxxxxxxxxx101xxxxx0110011
-`define SRLI   32'h0000000xxxxxxxxxx101xxxxx0010011
-`define SRA    32'h0100000xxxxxxxxxx101xxxxx0110011
-`define SRAI   32'h0100000xxxxxxxxxx101xxxxx0010011
+`define RV32_SLL    32'h0000000xxxxxxxxxx001xxxxx0110011  // *
+`define RV32_SLLI   32'h0000000xxxxxxxxxx001xxxxx0010011  // *
+`define RV32_SRL    32'h0000000xxxxxxxxxx101xxxxx0110011  // *
+`define RV32_SRLI   32'h0000000xxxxxxxxxx101xxxxx0010011  // *
+`define RV32_SRA    32'h0100000xxxxxxxxxx101xxxxx0110011  // *
+`define RV32_SRAI   32'h0100000xxxxxxxxxx101xxxxx0010011  // *
 // Arithmetic
-`define ADD    32'h0000000xxxxxxxxxx000xxxxx0110011
-`define ADDI   32'hxxxxxxxxxxxxxxxxx000xxxxx0010011
-`define SUB    32'h0100000xxxxxxxxxx000xxxxx0110011
-`define LUI    32'hxxxxxxxxxxxxxxxxxxxxxxxxx0110111
-`define AUIPC  32'hxxxxxxxxxxxxxxxxxxxxxxxxx0010111
+`define RV32_ADD    32'h0000000xxxxxxxxxx000xxxxx0110011  // *
+`define RV32_ADDI   32'hxxxxxxxxxxxxxxxxx000xxxxx0010011  // *
+`define RV32_SUB    32'h0100000xxxxxxxxxx000xxxxx0110011  // *
+`define RV32_LUI    32'hxxxxxxxxxxxxxxxxxxxxxxxxx0110111  // *
+`define RV32_AUIPC  32'hxxxxxxxxxxxxxxxxxxxxxxxxx0010111  // *
 // Logical
-`define XOR    32'h0000000xxxxxxxxxx100xxxxx0110011
-`define XORI   32'hxxxxxxxxxxxxxxxxx100xxxxx0010011
-`define OR     32'h0000000xxxxxxxxxx110xxxxx0110011
-`define ORI    32'hxxxxxxxxxxxxxxxxx110xxxxx0010011
-`define AND    32'h0000000xxxxxxxxxx111xxxxx0110011
-`define ANDI   32'hxxxxxxxxxxxxxxxxx111xxxxx0010011
+`define RV32_XOR    32'h0000000xxxxxxxxxx100xxxxx0110011  // *
+`define RV32_XORI   32'hxxxxxxxxxxxxxxxxx100xxxxx0010011  // *
+`define RV32_OR     32'h0000000xxxxxxxxxx110xxxxx0110011  // *
+`define RV32_ORI    32'hxxxxxxxxxxxxxxxxx110xxxxx0010011  // *
+`define RV32_AND    32'h0000000xxxxxxxxxx111xxxxx0110011  // *
+`define RV32_ANDI   32'hxxxxxxxxxxxxxxxxx111xxxxx0010011  // *
 // Compare
-`define SLT    32'h0000000xxxxxxxxxx010xxxxx0110011
-`define SLTI   32'hxxxxxxxxxxxxxxxxx010xxxxx0010011
-`define SLTU   32'h0000000xxxxxxxxxx011xxxxx0110011
-`define SLTIU  32'hxxxxxxxxxxxxxxxxx011xxxxx0010011
+`define RV32_SLT    32'h0000000xxxxxxxxxx010xxxxx0110011  // *
+`define RV32_SLTI   32'hxxxxxxxxxxxxxxxxx010xxxxx0010011  // *
+`define RV32_SLTU   32'h0000000xxxxxxxxxx011xxxxx0110011  // *
+`define RV32_SLTIU  32'hxxxxxxxxxxxxxxxxx011xxxxx0010011  // *
 // Branches
-`define BEQ    32'hxxxxxxxxxxxxxxxxx000xxxxx1100011
-`define BNE    32'hxxxxxxxxxxxxxxxxx001xxxxx1100011
-`define BLT    32'hxxxxxxxxxxxxxxxxx100xxxxx1100011
-`define BGE    32'hxxxxxxxxxxxxxxxxx101xxxxx1100011
-`define BLTU   32'hxxxxxxxxxxxxxxxxx110xxxxx1100011
-`define BGEU   32'hxxxxxxxxxxxxxxxxx111xxxxx1100011
+`define RV32_BEQ    32'hxxxxxxxxxxxxxxxxx000xxxxx1100011  // *
+`define RV32_BNE    32'hxxxxxxxxxxxxxxxxx001xxxxx1100011  // *
+`define RV32_BLT    32'hxxxxxxxxxxxxxxxxx100xxxxx1100011  // *
+`define RV32_BGE    32'hxxxxxxxxxxxxxxxxx101xxxxx1100011  // *
+`define RV32_BLTU   32'hxxxxxxxxxxxxxxxxx110xxxxx1100011  // *
+`define RV32_BGEU   32'hxxxxxxxxxxxxxxxxx111xxxxx1100011  // *
 // Jump & Link
-`define JAL    32'hxxxxxxxxxxxxxxxxxxxxxxxxx1101111
-`define JALR   32'hxxxxxxxxxxxxxxxxx000xxxxx1100111
+`define RV32_JAL    32'hxxxxxxxxxxxxxxxxxxxxxxxxx1101111  // *
+`define RV32_JALR   32'hxxxxxxxxxxxxxxxxx000xxxxx1100111  // *
 // Synch
-`define FENCE  32'h0000xxxxxxxx00000000000000001111
-`define FENCEI 32'h00000000000000000001000000001111
+`define RV32_FENCE  32'h0000xxxxxxxx00000000000000001111  // *
+`define RV32_FENCEI 32'h00000000000000000001000000001111  // *
 // CSR Access
-`define CSRRW  32'hxxxxxxxxxxxxxxxxx001xxxxx1110011
-`define CSRRS  32'hxxxxxxxxxxxxxxxxx010xxxxx1110011
-`define CSRRC  32'hxxxxxxxxxxxxxxxxx011xxxxx1110011
-`define CSRRWI 32'hxxxxxxxxxxxxxxxxx101xxxxx1110011
-`define CSRRSI 32'hxxxxxxxxxxxxxxxxx110xxxxx1110011
-`define CSRRCI 32'hxxxxxxxxxxxxxxxxx111xxxxx1110011
+`define RV32_CSRRW  32'hxxxxxxxxxxxxxxxxx001xxxxx1110011  // *
+`define RV32_CSRRS  32'hxxxxxxxxxxxxxxxxx010xxxxx1110011  // *
+`define RV32_CSRRC  32'hxxxxxxxxxxxxxxxxx011xxxxx1110011  // *
+`define RV32_CSRRWI 32'hxxxxxxxxxxxxxxxxx101xxxxx1110011  // *
+`define RV32_CSRRSI 32'hxxxxxxxxxxxxxxxxx110xxxxx1110011  // *
+`define RV32_CSRRCI 32'hxxxxxxxxxxxxxxxxx111xxxxx1110011  // *
 // Change Level
-`define ECALL  32'h00000000000000000000000001110011
-`define EBREAK 32'h00000000000100000000000001110011
-`define ERET   32'h00010000000000000000000001110011
-`define WFI    32'h00010000001000000000000001110011
+`define RV32_ECALL  32'h00000000000000000000000001110011  // *
+`define RV32_EBREAK 32'h00000000000100000000000001110011  // *
+`define RV32_ERET   32'h00010000000000000000000001110011  // *
+`define RV32_WFI    32'h00010000001000000000000001110011  // *
 // No Operation
-`define NOP    32'h00000000000000000000000000010011
+`define RV32_NOP    32'h00000000000000000000000000010011  // *
