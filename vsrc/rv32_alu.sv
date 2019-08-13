12b0e91952270eff8571729948d146772cf8f8ba
hossein1387 <m.h.askari.hemmat@gmail.com>
2019-08-12 23:28:47 -0400

    alu doing only certain operations

--- vsrc/rv32_alu.sv
+++ vsrc/rv32_alu.sv
@@ -1,38 +1,31 @@
 `include "types.vh"
 
 
-module alu (
+module rv32_alu (
     input  rv_register_t       rs1,
     input  rv_register_t       rs2,
-    input  rv_shamt_t          shamt,
-    input  rv_imm_t            imm,
-    input  rv32_opcode_enum_t  opcode
-    output rv_register_t       rd,
+    input  rv_alu_op_t         alu_opcode,
+    output rv_register_t       res,
+    output logic               z
 );
 
     always @(*) begin
-        case (rv_opcode)
-            RV32_SLL    : rd = rs1 << rs2;
-            RV32_SLLI   : rd = rs1 << shamt;
-            RV32_SRL    : rd = rs1 >> rs2;
-            RV32_SRLI   : rd = rs1 >> shamt;
-            RV32_SRA    : rd = $signed(rs1) >>> rs2;
-            RV32_SRAI   : rd = $signed(rs1) >>> shamt;
-            RV32_ADD    : rd = rs1 + rs2;
-            RV32_ADDI   : rd = rs1 + imm;
-            RV32_SUB    : rd = rs1 - rs2;
-            RV32_XOR    : rd = rs1 ^ rs2;
-            RV32_XORI   : rd = rs1 ^ imm;
-            RV32_OR     : rd = rs1 | rs2;
-            RV32_ORI    : rd = rs1 | imm;
-            RV32_AND    : rd = rs1 & rs2;
-            RV32_ANDI   : rd = rs1 & imm;
-            RV32_SLT    : rd = {31'b0, $signed(rs1) < $signed(rs2)};
-            RV32_SLTI   : rd = {31'b0, $signed(rs1) < $signed(imm)};
-            RV32_SLTU   : rd = {31'b0, rs1 < rs2};
-            RV32_SLTIU  : rd = {31'b0, rs1 < imm};
-            default     : rd = 0;
+        case (alu_opcode)
+            ALU_SLL     : res = rs1 << rs2;
+            ALU_SRL     : res = rs1 >> rs2;
+            ALU_SRA     : res = $signed(rs1) >>> rs2;
+            ALU_ADD     : res = rs1 + rs2;
+            ALU_SUB     : res = rs1 - rs2;
+            ALU_XOR     : res = rs1 ^ rs2;
+            ALU_OR      : res = rs1 | rs2;
+            ALU_AND     : res = rs1 & rs2;
+            ALU_SLT     : res = {31'b0, $signed(rs1) < $signed(rs2)};
+            ALU_SLTU    : res = {31'b0, rs1 < rs2};
+            ALU_EQ      : res = {31'b0, rs1==rs2};
+            default     : res = 0;
         endcase
     end
+
+    assign z = (res==0);
 
 endmodule
