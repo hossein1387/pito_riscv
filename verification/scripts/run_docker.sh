c45dd9fa340cfd84e10c01f7740a319ca215fc0c
hossein <m.h.askari.hemmat@gmail.com>
2019-12-14 00:46:11 -0500

    using upto 3 CPUs

--- verification/scripts/run_docker.sh
+++ verification/scripts/run_docker.sh
@@ -1,3 +1,3 @@
 xhost + 127.0.0.1
 open -a xQuartz
-docker run -it 	-h xilinx_container -e DISPLAY=docker.for.mac.localhost:0 -v /Volumes/MySD/sdx:/local/xilinx/ -v /Users/Hossein/MyRepos/pito_riscv/:/root/MyRepos/pito_riscv/ hossein1387/xilinx-tools-installed
+docker run -it --cpuset-cpus="0-3" -h xilinx_container -e DISPLAY=docker.for.mac.localhost:0 -v /Volumes/MySD/sdx:/local/xilinx/ -v /Users/Hossein/MyRepos/pito_riscv/:/root/MyRepos/pito_riscv/ hossein1387/xilinx-tools-installed
