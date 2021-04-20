54129ceaa9918abe7ec401cdb76647bdacade8e0
hossein1387 <m.h.askari.hemmat@gmail.com>
2021-04-20 17:43:26 -0400

    testing all rv32i tests in a script

---
+++ verification/tests/core/all_rv32i_tests.sh
@@ -0,0 +1,75 @@
+echo "Running add.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/add.hex >> log_add.log
+echo "Running andi.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/andi.hex >> log_andi.log
+echo "Running bge.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/bge.hex >> log_bge.log
+echo "Running bltu.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/bltu.hex >> log_bltu.log
+echo "Running jalr.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/jalr.hex >> log_jalr.log
+echo "Running lbu.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lbu.hex >> log_lbu.log
+echo "Running lui.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lui.hex >> log_lui.log
+echo "Running ori.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/ori.hex >> log_ori.log
+echo "Running sll.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sll.hex >> log_sll.log
+echo "Running slti.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/slti.hex >> log_slti.log
+echo "Running srl.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/srl.hex >> log_srl.log
+echo "Running sw.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sw.hex >> log_sw.log
+echo "Running xori.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/xori.hex >> log_xori.log
+echo "Running addi.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/addi.hex >> log_addi.log
+echo "Running auipc.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/auipc.hex >> log_auipc.log
+echo "Running bgeu.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/bgeu.hex >> log_bgeu.log
+echo "Running bne.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/bne.hex >> log_bne.log
+echo "Running j.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/j.hex >> log_j.log
+echo "Running lh.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lh.hex >> log_lh.log
+echo "Running lw.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lw.hex >> log_lw.log
+echo "Running sb.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sb.hex >> log_sb.log
+echo "Running slli.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/slli.hex >> log_slli.log
+echo "Running sra.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sra.hex >> log_sra.log
+echo "Running srli.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/srli.hex >> log_srli.log
+echo "Running test.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/test.hex >> log_test.log
+echo "Running and.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/and.hex >> log_and.log
+echo "Running beq.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/beq.hex >> log_beq.log
+echo "Running blt.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/blt.hex >> log_blt.log
+echo "Running jal.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/jal.hex >> log_jal.log
+echo "Running lb.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lb.hex >> log_lb.log
+echo "Running lhu.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/lhu.hex >> log_lhu.log
+echo "Running or.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/or.hex >> log_or.log
+echo "Running sh.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sh.hex >> log_sh.log
+echo "Running slt.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/slt.hex >> log_slt.log
+echo "Running srai.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/srai.hex >> log_srai.log
+echo "Running sub.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/sub.hex >> log_sub.log
+echo "Running xor.hex"
+./do_test.py -f files.f -t testbench_top -s xilinx -l libs.f -m vlogmacros.f --firmware=./firmwares/xor.hex >> log_xor.log
+
