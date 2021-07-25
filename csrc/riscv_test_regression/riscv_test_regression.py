954c0a4a346d9f0233b0fc0830aab2bc2ba43a2b
hossein1387 <m.h.askari.hemmat@gmail.com>
2021-07-24 21:00:53 -0400

    automated regresion for risc-v teest

---
+++ csrc/riscv_test_regression/riscv_test_regression.py
@@ -0,0 +1,68 @@
+import os, sys
+from shutil import copyfile
+from pathlib import Path
+import subprocess
+
+all_tests= ["addi", "and", "andi", "auipc", "beq", "bge", "bgeu", "blt", "bltu",
+"j", "jal", "jalr", "lb", "lbu", "lh", "lhu", "lui", "lw", "or", "ori", "sll", 
+"slli", "slt", "slti", "sra", "srai", "srl", "srli", "sub", "xor",
+"xori", "bne", "sw", "add"]
+
+# ommitted tests:
+# "sb", "sh",
+
+riscv_test_dir = "../asm_test/"
+cur_dir = os.getcwd()
+
+
+def run_command(command_str, split=True):
+        try:
+            print("running: {}".format(command_str))
+            # subprocess needs to receive args seperately
+            if split:
+                res = subprocess.call(command_str.split())
+            else:
+                res = subprocess.call(command_str, shell=True)
+            if res == 1:
+                print("Errors while executing: {0}".format(command_str))
+                sys.exit()
+        except OSError as e:
+            print("Unable to run {0} command".format(command_str))
+            sys.exit()
+
+def compile_tests():
+    for test in all_tests:
+        file_name = test + ".S"
+        test_file_path = riscv_test_dir + test + "/" + file_name
+        copyfile(test_file_path, cur_dir+"/"+file_name)
+        run_command("make {}.hex".format(test))
+
+def run_sim():
+    wd = os.getcwd()
+    os.chdir("/users/hemmat/MyRepos/pito_riscv/")
+    for test in all_tests:
+        print("Testsing {} ...".format(test))
+        cmd = "fusesoc run --target=sim pito --firmware=./csrc/riscv_test_regression/{}.hex > ./csrc/riscv_test_regression/{}.log".format(test, test)
+        run_command(cmd, False)
+    os.chdir(wd)
+
+def parse_logs():
+    for test in all_tests:
+        file_name = test + ".log"
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            lines = lines[::-1]
+            for line in lines:
+                if "RISC-V TEST Result:" in line:
+                    if "OK" in line:
+                        print("Test {:5s}: PASSED".format(test))
+                    elif "ERO" in line:
+                        print("Test {:5s}: FAILED".format(test))
+                    else:
+                        print("Test {:5s}: UNKMWON".format(test))
+                    break
+            f.close()
+
+compile_tests()
+run_sim()
+parse_logs()
