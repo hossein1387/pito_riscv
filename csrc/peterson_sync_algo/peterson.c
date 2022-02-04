2c94b8203412e1369c0cfd020ebb65c9b458cab6
hossein1387 <m.h.askari.hemmat@gmail.com>
2022-02-04 14:05:13 -0500

    working version of peterson mechanism on pito

--- csrc/peterson_sync_algo/peterson.c
+++ csrc/peterson_sync_algo/peterson.c
@@ -3,21 +3,21 @@
 #define N 8
 extern int get_pito_hart_id();
 
-int level[N]={0};
-int last_to_enter[N]={0};
+volatile int level[N]={0};
+volatile int last_to_enter[N]={0};
 
-void main_thread(int i){
-    int j, k, l, do_wait;
+void main_thread(int hart_id){
+    volatile int j, k, l, do_wait;
 //     printf("Thread %d/%d beginning execution!\n", i, N);
     
     for(j=0;j<1000000;j++){
         for(l=0 ; l<N-1; l++){
-            level[i]         = l;
-            last_to_enter[l] = i;
+            level[hart_id]         = l;
+            last_to_enter[l] = hart_id;
             do{
-                do_wait = last_to_enter[l] == i;
+                do_wait = last_to_enter[l] == hart_id;
                 for(k=0; do_wait && k<N;k++){
-                    if(k == i) continue;
+                    if(k == hart_id) continue;
                     if(level[k] >= l){  
                         break;
                     }
@@ -25,6 +25,24 @@ void main_thread(int i){
                 do_wait = do_wait && k<N;
             }while(do_wait);
         }
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
+        __asm__ volatile ("nop"::);
 //         fprintf(stdout, "Thread %d entered critical section... \"", i);
 //         fflush (stdout);
         
@@ -35,7 +53,7 @@ void main_thread(int i){
 //         /* Exit critical section */
 //         fprintf(stdout, "\"  Thread %d exiting critical section.\n", i);
 //         fflush (stdout);
-        level[i] = -1;
+        level[hart_id] = -1;
     }
     
     
